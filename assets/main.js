870c6b6110eb313e95dd3109e64c495f099a1ec6
Sérgio Leitão <sergiomiranda86@gmail.com>
2024-03-07 18:36:01 -0300

    ajustando função redirect para página de obrigado

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,16 @@
-function sendEmail() { Email.send({ Host: "smtp.gmail.com", Username: "desventurasdeumaltergo@gmail.com", Password: "desventuras@&9002", To: "sergiomiranda86@gmail.com", From: document.getElementById("emailData").value, Subject: "Novo lead do curso Aprenda do Zero da empresa LOGO", Body: "Temos um novo lead: " + document.getElementById("emailData").value }).then((message => alert("Lead enviado com sucesso!")), document.getElementById("emailForm").addEventListener("submit", (function (event) { event.preventDefault(); window.location.href = "/pages/thankyou.html" }))) }
+function sendEmail() { 
+  Email.send({ 
+    Host: "smtp.gmail.com", 
+    Username: "desventurasdeumaltergo@gmail.com", 
+    Password: "desventuras@&9002", 
+    To: "sergiomiranda86@gmail.com", 
+    From: document.getElementById("emailData").value, 
+    Subject: "Novo lead do curso Aprenda do Zero da empresa LOGO", 
+    Body: "Temos um novo lead: " + document.getElementById("emailData").value 
+  })
+  .then((message => alert("Lead enviado com sucesso!"))
+)}
+
+document.getElementById("emailForm").addEventListener("submit", (function (event) { 
+  event.preventDefault(); window.location.href = "/pages/thankyou.html" 
+}))
