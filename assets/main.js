7a2135940aa556150d586518ced5316b6fa05414
Sérgio Leitão <sergiomiranda86@gmail.com>
2024-03-07 23:53:17 -0300

    atualização da lógica da atualização para página de obrigado

--- assets/main.js
+++ assets/main.js
@@ -1,46 +1,48 @@
-const emailLead = document.getElementById('emailData');
-
 // Adicionei evento Listerner no formulário
-const submit = document.getElementById('emailForm');
+const submit = document.getElementsByClassName('formContact')[0];
 submit.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log("Clicado")
-  
+
   /* fonte: https://smtpjs.com/ */
-  Email.send({
+  /* Não funcionou com SecureToken */
+  /* Email.send({
     SecureToken : "8ff6fa24-f4b4-41bc-9d5c-cff468159d0e",
     To : 'sergiomiranda86@gmail.com',
     From : "desventurasdeumaltergo@gmail.com",
     Subject: "Sua inscrição foi feita com sucesso!", 
-    Body: "Esperamos você no dia 31/02/2024 para o nosso encontro, Aprenda do Zero da empresa LOGO"
+    Body: "<h3>Esperamos você no dia 31/02/2024 para o nosso encontro, Aprenda do Zero da empresa LOGO</h3>"
   }).then(
     message => alert(message)
-  ); 
+  );  */
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "desventurasdeumaltergo@gmail.com",
+    Password: "E3246C7C8016878F6E6FE77631F148964952",
+    From: 'desventurasdeumaltergo@gmail.com',
+    To: document.getElementById('emailData').value,
+    Subject: "Sua inscrição foi feita com sucesso!",
+    Body: "<h3>Esperamos você no dia 31/02/2024 para o nosso encontro, Aprenda do Zero da empresa LOGO</h3>"
+  }).then(
+    message => alert(message)
+  );
 })
 
-/* const protocolo = window.location.protocol
-console.log("Protocolo: ", protocolo)
-
+const protocolo = window.location.protocol
 const dominio = window.location.hostname
+const port = window.location.port
 const baseUrlLocal = '/meu-portfolio-projetos/teste-capturelead/pages/thankyou.html';
-console.log(dominio + baseUrlLocal)
-
-const base = window.location.href
 const baseUrlProd = '/teste-capturelead/pages/thankyou.html';
-
-document.getElementById("emailForm").addEventListener("submit", (function (event) { 
-  event.preventDefault();
-  if (dominio === '127.0.0.1') {
-    window.location.href = dominio + baseUrlLocal;
-  } else {
-    window.location.href = dominio+baseUrlProd;
+const urlLocal = (protocolo + "//" + dominio + ":" + port + baseUrlLocal)
+console.log(urlLocal)
+const urlProd = (protocolo + "//" + dominio + baseUrlProd)
+console.log(urlProd)
+document.getElementById("emailForm").addEventListener("submit", ((e) => {
+    e.preventDefault();
+    if (dominio === '127.0.0.1') {
+      window.location.href = urlLocal;
+    } else {
+      window.location.href = urlProd;
+    }
   }
-} */
-
-/* document.getElementById("emailForm").addEventListener("submit", (function (event) { 
-  event.preventDefault();
-  window.location.href = 'https://seralterego.github.io/teste-capturelead/pages/thankyou.html';
-}))
-console.log(e) */
-
-/* Validação campo e-mail */
+))
